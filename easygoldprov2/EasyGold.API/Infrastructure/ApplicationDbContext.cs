1383800f926cc14d2a71aabc97b804ccdbce5eda
CamillaTomorrow1 <camilla@tomorrowsrl.com>
2025-06-16 17:40:47 +0200

    Add DbSet for new anagrafiche entities

--- easygoldprov2/EasyGold.API/Infrastructure/ApplicationDbContext.cs
+++ easygoldprov2/EasyGold.API/Infrastructure/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ using EasyGold.Web2.Models.Cliente.Entities.Contabilita;
 using EasyGold.Web2.Models.Cliente.Entities.Contabilita;
 using EasyGold.Web2.Models.Cliente.Entities.ACL;
 using EasyGold.Web2.Models.Cliente.Entities.ACL;
+using EasyGold.Web2.Models.Cliente.Entities.Anagrafiche;
 using EasyGold.Web2.Models.Comune.Entities;
 
 namespace EasyGold.API.Infrastructure
@@ -40,6 +41,8 @@ namespace EasyGold.API.Infrastructure
         public DbSet<DbNumeriRegIVA> NumeriRegIVA { get; set; }
         public DbSet<DbConfig> Configurazioni { get; set; }
         public DbSet<DbConfigLang> ConfigLag { get; set; }
+        public DbSet<DbDocumentoCliente> DocumentiCliente { get; set; }
+        public DbSet<DbImpresaFinanziaria> ImpreseFinanziarie { get; set; }
 
         /// <summary>
         /// Configura le entità e le relazioni tra di esse.
@@ -54,6 +57,8 @@ namespace EasyGold.API.Infrastructure
             modelBuilder.Entity<DbModuloEasygold>().HasKey(m => m.Mde_IDAuto);
             modelBuilder.Entity<DbNazioni>().HasKey(n => n.Ntn_ISO1);
             modelBuilder.Entity<DbValute>().HasKey(v => v.Val_id);
+            modelBuilder.Entity<DbDocumentoCliente>().HasKey(d => d.Doc_IdAuto);
+            modelBuilder.Entity<DbImpresaFinanziaria>().HasKey(i => i.Imf_IdAuto);
 
             modelBuilder.Entity<DbUtente>()
             .HasOne(u => u.Ruolo)
