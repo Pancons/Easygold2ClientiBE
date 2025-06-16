e32893c4ff068789f4de444eb201b523f95cf79e
Ubuntu <ubuntu@ip-172-31-29-111.eu-south-1.compute.internal>
2025-06-16 14:21:11 +0000

    Partenza Camilla

--- EasyGold.Web2.Models/Models/Cliente/Allegati/AllegatoDTO.cs
+++ EasyGold.Web2.Models/Models/Cliente/Allegati/AllegatoDTO.cs
@@ -2,7 +2,7 @@ using Swashbuckle.AspNetCore.Annotations;
 using System.ComponentModel.DataAnnotations;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Security.Principal;
-
+//CIAO
 
 namespace EasyGold.Web2.Models.Cliente.Allegati
 {
